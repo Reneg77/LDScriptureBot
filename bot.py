09174d6d41d89f17d90bac2290489be45fbe60f0
Reneg <47455537+Reneg77@users.noreply.github.com>
2023-06-15 11:26:34 -0600

    added checks and messages for misinputs

--- bot.py
+++ bot.py
@@ -70,6 +70,7 @@ async def on_ready():
 async def on_message(message: discord.message):
     content = message.clean_content
     if '[' and ']' in content:
+        misinputs = []
         text = ''
         for start in range(0, len(content)):
             if content[start] == '[':
@@ -78,6 +79,7 @@ async def on_message(message: discord.message):
                     if content[end] == ']' and not found:
                         found = True
                         verses = content[start+1:end]
+                        input = verses
                         if '-' in verses:
                             for char in range(0, len(verses)):
                                 if verses[char] == ' ' or verses[char] == ':':
@@ -89,39 +91,56 @@ async def on_message(message: discord.message):
                             result = ''
                             for number in range(int(start_No), int(end_No)+1):
                                 verse = verses[0:start_Index+1] + str(number)
-                                result += '\n' + str(number) + '.  ' + get_verse(verse)
+                                result += get_verse(verse)
+                                if result != '':
+                                    result = '\n' + str(number) + '.  ' + result
                             if result != '':
                                 if text != '':
                                     text += '\n\n'
                                 text += verses + ':' + result
+                            else:
+                                misinputs += [input]
                         else:
                             result = get_verse(verses[0:len(verses)])
                             if result != '':
                                 if text != '':
                                     text += '\n\n'
                                 text += verses + ':\n' + result
-        embed = discord.Embed()
-        embed.color = discord.Color.from_rgb(15, 110, 150)
-        embed.title = 'References:'
-        if len(text) > 4096:
-            messages = []
-            count = 0
-            max = int(len(text)/4096)
-            while len(text) > 4096:
-                count += 1
-                embed.description = text[0:4096]
-                embed.title = 'References: (' + str(count) + '/' + str(max) + ')'
-                await message.channel.send(embed = embed)
-                text = text[4096:len(text)]
-            embed.description = text
-            embed.title = 'References: (' + str(max) + '/' + str(max) + ')'
+                            else:
+                                misinputs += [input]
+
+        if text != '':
+            embed = discord.Embed()
+            embed.color = discord.Color.from_rgb(15, 110, 150)
+            embed.title = 'References:'
+            if len(text) > 4096:
+                messages = []
+                count = 0
+                max = int(len(text)/4096)
+                while len(text) > 4096:
+                    count += 1
+                    embed.description = text[0:4096]
+                    embed.title = 'References: (' + str(count) + '/' + str(max) + ')'
+                    await message.channel.send(embed = embed)
+                    text = text[4096:len(text)]
+                embed.description = text
+                embed.title = 'References: (' + str(max) + '/' + str(max) + ')'
+                await message.channel.send(embed=embed)
+            else:
+                embed.description = text
+                await message.channel.send(embed=embed)
+
+        if len(misinputs) > 0:
+            embed = discord.Embed()
+            embed.color = discord.Color.from_rgb(150, 50, 50)
+            embed.title = 'Hmm,'
+            if len(misinputs) > 1:
+                embed.description = f'I couldn\'t find that. Check for mispellings in the following inputs:'
+            else:
+                embed.description = f'I couldn\'t find that. Check for mispellings in the following input:'
+            for misinput in misinputs:
+                embed.description += f'\n*{misinput}*'
             await message.channel.send(embed=embed)
-        else:
-            embed.description = text
-            await message.channel.send(embed=embed)
 
-
-
-        
 
 client.run(TOKEN)
